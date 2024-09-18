61545891e0ed34d6ac26efdf049eff8012adcd42
Andreas Georgiou <geo.andreasyeah@gmail.com>
2024-09-19 00:58:11 +0200

    Added cloak

--- scripts/entrypoint-cloak-server.sh
+++ scripts/entrypoint-cloak-server.sh
@@ -4,27 +4,9 @@ echo -e "=======================================================================
 echo -e "========================= CLOAK DOMAIN FRONTING =============================="
 echo -e "=============================================================================="
 
-sed -i "s|LOCAL_IP|${LOCAL_IP}|" /app/config.json
-sed -i "s|LOCAL_PORT|${LOCAL_PORT}|" /app/config.json
-sed -i "s|BYPASSUID|${BYPASSUID}|" /app/config.json
-sed -i "s|REDIRADDR|${REDIRADDR}|" /app/config.json
-sed -i "s|PRIVATEKEY|${PRIVATEKEY}|" /app/config.json
-sed -i "s|ADMINUID|${ADMINUID}|" /app/config.json
-sed -i "s|SERVERNAME|${REDIRADDR}|" /app/config.json
-sed -i "s|BINDPORT|${BINDPORT}|" /app/config.json
-
-echo -e '[+] Config.json generated successfully.'
-echo -e '[+] Show Container config'
-echo -e "[!] Local IP : \t\t${LOCAL_IP}"
-echo -e "[!] Local Port : \t${LOCAL_PORT}"
-echo -e "[!] Method : \t\t${METHOD}"
-echo -e "[!] BypassUID : \t${BYPASSUID}"
-echo -e "[!] Server Name : \t${REDIRADDR}"
-echo -e "[!] PrivateKey : \t${PRIVATEKEY}"
-echo -e "[!] AdminUID : \t\t${ADMINUID}"
-echo -e "[!] PublicKey : \t${PUBLICKEY}"
-echo -e "[!] SS Encryption : \t${ENCRYPTION}"
-echo -e "[!] SS Password : \t${PASSWORD}"
-echo -e "[+] Happy Domain Fronting :)"
+sed -i "s|<CLOAK_BIND_PORTS>|${CLOAK_BIND_PORTS}|" /app/config.json
+sed -i "s|<CLOAK_BYPASS_UID>|${CLOAK_BYPASS_UID}|" /app/config.json
+sed -i "s|<CLOAK_REDIR_ADDR>|${CLOAK_REDIR_ADDR}|" /app/config.json
+sed -i "s|<CLOAK_PRIVATE_KEY>|${CLOAK_PRIVATE_KEY}|" /app/config.json
 
 exec "$@"
